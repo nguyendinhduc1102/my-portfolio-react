352d974ded8a4d8c6ca6662efc27568c5fbc23cc
nguyendinhduc1102 <nguyendinhduc29041999@gmail.com>
2020-10-29 14:39:53 +0700

    inital commit

---
+++ src/yourdata.js
@@ -0,0 +1,51 @@
+export default
+    {
+       
+        name :'NguyenDinhDuc.',
+        headerTagline: [
+                        'Hi 👋 You found me! I am Nguyen Dinh Duc.',
+                        
+                        'Fullstack web developer ',
+                        ''
+    ],
+        
+        contactEmail:'nguyendinhducbg2904@gmail.com',
+        
+        abouttext: "My name is Nguyen Dinh Duc.I am currently a student in the IT department of the last year of Hactech. As a programming student, he wishes to work in a professional environment to improve skills, accumulate, learn practical experience and knowledge from colleagues in company.",
+        aboutImage:'',
+       
+       ShowAboutImage:true,
+
+       projects:[
+           {
+            id: 1,
+            title:'Web Farm',
+             service:'PHP',
+             imageSrc:"/images/project-farm-ew.png",
+             
+             url:'https://farm-e-w.000webhostapp.com/'
+            },
+           {
+            id: 2,
+            title:'Generator color',
+             service:'Reactjs',
+             imageSrc:"/images/generator-color.png",
+             
+             url:'https://farm-e-w.000webhostapp.com/'
+            },
+        ],
+        social: [
+            
+            {   name:'Github',
+                url:'https://github.com/nguyendinhduc1102'},
+            {
+                name: 'Facebook',
+                url: 'https://www.facebook.com/profile.10001102/'
+            },
+            {
+                name: 'Email',
+                url: 'nguyendinhducbg2904@gmail.com'
+            }
+
+        ]
+    }
